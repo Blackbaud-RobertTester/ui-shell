fa5de69749710d4e80dd151f54624a3a5d7931a9
Blackbaud-RobertTester <robert.tester@blackbaud.com>
2015-10-01 14:41:37 -0500

    Module Pattern

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,18 +1,17 @@
-/* global malarkey:false, moment:false */
+/*global moment*/
 
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
-import { MainController } from './main/main.controller';
-import { ExampleService } from './main/example_service.js'
-import {HelloWorldTitle} from './main/example_directive.js'
+/*eslint-disable no-unused-vars*/
+import { mainModule } from './main/main.module'
+/*eslint-enable no-unused-vars*/
 
-angular.module('uiShell', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'toastr'])
-    .constant('malarkey', malarkey)
+angular.module('uiShell', ['restangular', 'ui.router', 'uiShell.main'])
     .constant('moment', moment)
     .config(config)
     .config(routerConfig)
-    .run(runBlock)
-    .controller('MainController', MainController)
-    .directive('helloWorldTitle', () => new HelloWorldTitle())
-    .service('ExampleService', ExampleService);
+    .run(runBlock);
+
+
+
